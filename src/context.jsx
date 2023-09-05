51723273adb0aaae4394d3515494cb43aae25079
codymcnamara <codyjamesmcnamara@gmail.com>
2023-09-05 12:31:17 -0700

    add increase functionality

--- src/context.jsx
+++ src/context.jsx
@@ -31,8 +31,12 @@ function AppProvider ({children}){
         dispatch({type: REMOVE, payload: {id}})
     }
 
+    const increaseItem = (id) => {
+        dispatch({type: INCREASE, payload: {id}});
+    }
+
     return (
-        <AppContext.Provider value={{...state, clearCart, removeItem}}>
+        <AppContext.Provider value={{...state, clearCart, removeItem, increaseItem}}>
             {children}
         </AppContext.Provider>
     )
--- src/reducer.js
+++ src/reducer.js
@@ -10,14 +10,23 @@ import {
 const reducer = (state, action) => {
     
     if(action.type === REMOVE){
-        const newCart = new Map(state.cart);
-        newCart.delete(action.payload.id);
+        const newCart= new Map(state.cart);
         return {...state, cart: newCart };
 
     }
 
     if (action.type === CLEAR_CART){
         return {...state, cart: new Map() };
+    }
+
+    if (action.type === INCREASE) {
+        const newCart = new Map(state.cart);
+        let itemId = action.payload.id;
+        let item = newCart.get(itemId);
+        let newItem = {...item, amount: item.amount + 1}
+        newCart.set(itemId, newItem);
+
+        return {...state, cart: newCart };
     }
     
     throw new Error(`No matching "${action.type}" - action type`);
